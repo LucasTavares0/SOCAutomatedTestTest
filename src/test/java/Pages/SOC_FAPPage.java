52d1411b14c50976a6d1275c84001e8c0982ffb4
LucasTavares0 <lucas.tavares@modalgr.com.br>
2019-11-18 15:34:43 -0200

    Creating default packages and creating HomePage class and FAP Page class

---
+++ src/test/java/Pages/SOC_FAPPage.java
@@ -0,0 +1,6 @@
+package Pages;
+
+public class SOC_FAPPage {
+	
+	//URL = https://ww2.soc.com.br/fap/
+}
---
+++ src/test/java/Pages/SOC_homePage.java
@@ -0,0 +1,6 @@
+package Pages;
+
+public class SOC_homePage {
+	
+	//URL = https://ww2.soc.com.br/blog/
+}
