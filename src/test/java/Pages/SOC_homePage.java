7b62b2e752e37460a8902c6cde2493476b389085
LucasTavares0 <lucas.tavares@modalgr.com.br>
2019-11-21 01:57:45 -0200

    Creating ResultadosBuscaPage and updating HomePage

--- src/test/java/Pages/SOC_homePage.java
+++ src/test/java/Pages/SOC_homePage.java
@@ -1,6 +1,38 @@
 package Pages;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 public class SOC_homePage {
 	
-	//URL = https://ww2.soc.com.br/blog/
+	WebDriver driver;
+	
+	//WebElements Declaration
+	@FindBy(css = "input[type='text'][placeholder='Buscar']")
+	WebElement BuscarTextBox;
+	
+	@FindBy(css = "input[type='submit'][class='lupa-form']")
+	WebElement BuscarButton;
+	
+	@FindBy(css = "a[href='/fap']")
+	WebElement FAPLink;
+	
+	//Action Methods
+	public void sendKeysBuscarTextBox(String searchFilter) {
+		BuscarTextBox.sendKeys(searchFilter);
+	}
+	public void clickOnBuscarButton() {
+		BuscarButton.click();
+	}
+	public void clickOnLinkFAP() {
+		FAPLink.click();
+	}
+	
+	//Constructor
+	public SOC_homePage(WebDriver driver) {
+		this.driver = driver;
+		PageFactory.initElements(driver, this);
+	}
 }
