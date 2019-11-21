7be79408a5bce8bfb0faabddb0ae14f6f99362e3
LucasTavares0 <lucas.tavares@modalgr.com.br>
2019-11-21 13:55:33 -0200

    Creating glue code for feature FAP.feature

--- features/FAP.feature
+++ features/FAP.feature
@@ -6,11 +6,11 @@ Feature: Test the FAP calculator functionality
 
 Scenario: User should be able to insert valid values and get a FAP calculation report
 
-Given: user is on the FAP page
-When: user enters the company name 
-And: user enters a FAP value
-And: user enters a RAT percentage
-And: user enters a salary projection
-And: user clicks on Estimar FAP button
-Then: user receive a FAP calculation report 
+Given user is on the FAP page
+When user enters the company name 
+And user enters a FAP value
+And user enters a RAT percentage
+And user enters a salary projection
+And user clicks on Estimar FAP button
+Then user receive a FAP calculation report 
 
---
+++ src/test/java/StepsImplementation/BDDFapTest.java
@@ -0,0 +1,44 @@
+package StepsImplementation;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class BDDFapTest {
+	
+	@Given("^user is on the FAP page$")
+	public void user_is_on_the_FAP_page(){
+		
+	}
+
+	@When("^user enters the company name$")
+	public void user_enters_the_company_name(){
+		
+	}
+
+	@And("^user enters a FAP value$")
+	public void user_enters_a_FAP_value(){
+		
+	}
+
+	@And("^user enters a RAT percentage$")
+	public void user_enters_a_RAT_percentage(){
+		
+	}
+
+	@And("^user enters a salary projection$")
+	public void user_enters_a_salary_projection(){
+		
+	}
+
+	@And("^user clicks on Estimar FAP button$")
+	public void user_clicks_on_Estimar_FAP_button(){
+		
+	}
+
+	@Then("^user receive a FAP calculation report$")
+	public void user_receive_a_FAP_calculation_report (){
+		
+	}
+}
