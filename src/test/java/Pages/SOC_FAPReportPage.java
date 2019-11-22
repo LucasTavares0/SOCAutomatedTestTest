9f3e57d203787e9debc245e420c03d2ee79ae6bf
LucasTavares0 <lucas.tavares@modalgr.com.br>
2019-11-22 02:24:33 -0200

    Update implementing FAPReport page model

--- src/test/java/Pages/SOC_FAPReportPage.java
+++ src/test/java/Pages/SOC_FAPReportPage.java
@@ -1,6 +1,8 @@
 package Pages;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class SOC_FAPReportPage {
@@ -8,13 +10,92 @@ public class SOC_FAPReportPage {
 	WebDriver driver;
 	
 	//WebElements Declaration
+	@FindBy(xpath = "//*[@id=\"page\"]/section/div/header/h1")
+	WebElement tituloRelatorioLabel;
 	
+	@FindBy(xpath = "//*[@id=\"page\"]/section/div/article[1]/table/tbody/tr/td[1]/span")
+	WebElement nomeEmpresaLabel;
 	
+	@FindBy(xpath = "//*[@id=\"page\"]/section/div/article[1]/table/tbody/tr/td[2]/span")
+	WebElement projecaoSalarialLabel;
+	
+	@FindBy(xpath = "//*[@id=\"page\"]/section/div/article[1]/table/tbody/tr/td[3]/span")
+	WebElement ratLabel;
+	
+	@FindBy(xpath = "//*[@id=\"page\"]/section/div/article[1]/table/tbody/tr/td[4]/span")
+	WebElement fapLabel;
+	
+	@FindBy(xpath = "//*[@id=\"page\"]/section/div/article[2]/table/tbody/tr[1]/td[3]/span")
+	WebElement recolhimentoRatProjetadoLabel;
+	
+	@FindBy(xpath = "//*[@id=\"page\"]/section/div/article[2]/table/tbody/tr[2]/td[3]/span")
+	WebElement recolhimentRatAjustadoProjetadoLabel;
+	
+	@FindBy(xpath = "//*[@id=\"page\"]/section/div/article[2]/table/tbody/tr[3]/td[2]/span")
+	WebElement impactoAnualProjetadoLabel;
+	
+	@FindBy(xpath = "//*[@id=\"page\"]/section/div/article[3]/table/tbody/tr[1]/td[3]/span")
+	WebElement recolhimentoRatAlcancarLabel;
+	
+	@FindBy(xpath = "//*[@id=\"page\"]/section/div/article[3]/table/tbody/tr[2]/td[3]/span")
+	WebElement recolhimentRatAjustadoAlcancarLabel;
+	
+	@FindBy(xpath = "//*[@id=\"page\"]/section/div/article[3]/table/tbody/tr[3]/td[2]/span")
+	WebElement impactoAnualAlcancarLabel;
+	
+	@FindBy(xpath = "//*[@id=\"page\"]/section/div/article[4]/table/tbody/tr/td[1]/span")
+	WebElement recolhimentoMinimoLabel;
+	
+	@FindBy(xpath = "//*[@id=\"page\"]/section/div/article[4]/table/tbody/tr/td[2]/span")
+	WebElement recolhimentoMaximoLabel;
+	
+	@FindBy(xpath = "//*[@id=\"page\"]/section/div/article[4]/table/tbody/tr/td[3]/span")
+	WebElement recolhimentoPassivelEconomiaLabel;
 	
 	
 	//Action Methods
-	
-	
+	public String getTituloRelatorioText() {
+		return tituloRelatorioLabel.getText();
+	}
+	public String getNomeEmpresaText() {
+		return nomeEmpresaLabel.getText();
+	}
+	public String getProjecaoSalariaText() {
+		return projecaoSalarialLabel.getText();
+	}
+	public String getRatText() {
+		return ratLabel.getText();
+	}
+	public String getFapText() {
+		return fapLabel.getText();
+	}
+	public String getRecolhimentoRatProjetadoText() {
+		return recolhimentoRatProjetadoLabel.getText();
+	}
+	public String getRecolhimentoRatAjustadoProjetadoText() {
+		return recolhimentRatAjustadoProjetadoLabel.getText();
+	}
+	public String getImpactoAnualProjetadoText() {
+		return impactoAnualProjetadoLabel.getText();
+	}
+	public String getRecolhimentoRatAlcancarText() {
+		return recolhimentoRatAlcancarLabel.getText();
+	}
+	public String getRecolhimentoRatAjustadoAlcancarText() {
+		return recolhimentRatAjustadoAlcancarLabel.getText();
+	}
+	public String getImpactoAnualAlcancarText() {
+		return impactoAnualAlcancarLabel.getText();
+	}
+	public String getRecolhimentoMinimoText() {
+		return recolhimentoMinimoLabel.getText();
+	}
+	public String getRecolhimentoMaximoText() {
+		return recolhimentoMaximoLabel.getText();
+	}
+	public String getRecolhimentoPassivelEconomiaText() {
+		return recolhimentoPassivelEconomiaLabel.getText();
+	}
 	
 	//Constructor
 	public SOC_FAPReportPage(WebDriver driver) {
